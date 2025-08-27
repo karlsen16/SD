9034668d6fa24e7786da63c60b2e17fbe5deb39b
lrdt <l.rdt@pm.me>
2025-08-27 13:28:41 -0300

    Assinatura e verificação

--- generateKeys.py
+++ generateKeys.py
@@ -6,12 +6,7 @@ key = RSA.generate(2048)
 private_key = key.export_key(format='DER')
 with open("./cliente/private_key.der", "wb") as f:
     f.write(private_key)
-with open("./servidor/private_key.der", "wb") as f:
-    f.write(private_key)
-
 
 public_key = key.publickey().export_key(format='DER')
 with open("./cliente/public_key.der", "wb") as f:
-    f.write(public_key)
-with open("./servidor/public_key.der", "wb") as f:
     f.write(public_key)
